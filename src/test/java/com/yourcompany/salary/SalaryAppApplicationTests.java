509fa6cfe3112d9ca382c0aac0fe98d141d1fe6a
Asangali Yergali <yergali.asangali@gmail.com>
2025-06-23 00:43:41 +0500

    chore: initial project skeleton with H2 and basic security

---
+++ src/test/java/com/yourcompany/salary/SalaryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcompany.salary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
